e2518a7ac193a0d1923bb5d76a8b8e25385e5ba9
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-26 21:11:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,10 @@
 class Application
   def call(env) 
     resp = Rack::Response.new 
-    
+    if Time.now < 12
+      resp.write 'Good Morning!'
+    else 
+      resp.write 'Good Afternoon!'
+    end 
   end 
 end 
